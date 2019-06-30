a01571e5f584b7c035aa7a4ec32250c9d2dcb02c
Artemy Lance <artemylance@ya.ru>
2019-07-01 00:19:54 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,20 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      if (pathname.split('/').length > 1) {
+        res.statusCode = 400;
+        res.end('Bad request');
+      } else {
+        fs.stat(filepath, (err, stats) => {
+          if (err || !stats.isFile()) {
+            res.statusCode = 404;
+            res.end('File not found');
+          } else {
+            const fileStream = fs.createReadStream(filepath);
+            fileStream.pipe(res);
+          }
+        });
+      }
       break;
 
     default:

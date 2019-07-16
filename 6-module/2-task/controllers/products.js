1a7ded36f1a7a361d1b59b55793666f1ba1c6a80
Artemy Lance <artemylance@ya.ru>
2019-07-17 00:28:55 +0300

    add solution for 6-module 2-task mongoose error

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Product = require('../models/Product');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
@@ -25,16 +26,16 @@ module.exports.productList = async function productList(ctx, next) {
 
 module.exports.productById = async function productById(ctx, next) {
   const id = ctx.params.id;
-
-  const product = await Product.findOne({'_id': id}).catch(() => {
-    ctx.throw(400, 'invalid id');
-  });
-
-  if (!product) {
-    ctx.status = 404;
-    return;
+  try {
+    const product = await Product.findOne({'_id': id});
+    ctx.body = {product: {...product, id: product._id}};
+  } catch (err) {
+    if (err instanceof mongoose.CastError) {
+      ctx.throw(400, 'invalid id');
+    } else {
+      ctx.throw(404, 'Product not found');
+    }
   }
 
-  ctx.body = {product: {...product, id: product._id}};
   next();
 };

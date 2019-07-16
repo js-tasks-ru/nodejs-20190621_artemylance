e2c444b3093eea3d30bd4f1154918e6844b864cb
Artemy Lance <artemylance@ya.ru>
2019-07-17 00:14:04 +0300

    add solution for 6-module 2-task products

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,48 @@
+const Product = require('../models/Product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {products: []};
+  const products = await Product.find({subcategory: ctx.query.subcategory}).then((list) => {
+    return list.map((product) => ({
+      id: product._id,
+      title: product.title,
+      description: product.description,
+      images: product.images,
+      price: product.price,
+      category: product.category,
+      subcategory: product.subcategory,
+    }));
+  }).catch(()=>[]);
+
+  ctx.body = {products};
+  next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {products: []};
+  const products = await Product.find({});
+  ctx.body = {products};
+  next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {product: {}};
-};
+  const id = ctx.params.id;
 
+  const product = await Product.findOne({'_id': id}).catch(() => {
+    ctx.throw(400, 'invalid id');
+  });
+
+  if (!product) {
+    ctx.status = 404;
+    return;
+  }
+
+  ctx.body = {product: {
+    id: product._id,
+    title: product.title,
+    description: product.description,
+    images: product.images,
+    price: product.price,
+    category: product.category,
+    subcategory: product.subcategory,
+  }};
+  next();
+};
